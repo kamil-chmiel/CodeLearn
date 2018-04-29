193db4b08107c68a5faa1430232efc3c86424b7d
mdjdrn1 <mdjdrn1@gmail.com>
2018-04-29 21:57:44 +0200

    Bound buttons with activity classes members.

--- app/src/main/java/com/example/kamil/codelearn/SignInActivity.java
+++ app/src/main/java/com/example/kamil/codelearn/SignInActivity.java
@@ -1,13 +1,28 @@
 package com.example.kamil.codelearn;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignInActivity extends AppCompatActivity {
+    private EditText mailTextField;
+    private EditText passwordTextField;
+
+    private Button signInBasicButton;
+    private Button signInFacebookButton;
+    private Button signInGoogleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        mailTextField = findViewById(R.id.signInEmailText);
+        passwordTextField = findViewById(R.id.signInPasswordText);
+
+        signInBasicButton = findViewById(R.id.signInBasicButton);
+        signInFacebookButton = findViewById(R.id.signInFacebookButton);
+        signInGoogleButton = findViewById(R.id.signInGoogleButton);
     }
 }
--- app/src/main/java/com/example/kamil/codelearn/SignUpActivity.java
+++ app/src/main/java/com/example/kamil/codelearn/SignUpActivity.java
@@ -2,12 +2,29 @@ package com.example.kamil.codelearn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+    private EditText mailTextField;
+    private EditText passwordTextField;
+    private EditText repeatPasswordTextField;
+
+    private Button signUpBasicButton;
+    private Button signUpFacebookButton;
+    private Button signUpGoogleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mailTextField = findViewById(R.id.signUpEmailText);
+        passwordTextField = findViewById(R.id.signUpPasswordText);
+        passwordTextField = findViewById(R.id.signUpRepeatPasswordText);
+
+        signUpBasicButton = findViewById(R.id.signUpBasicButton);
+        signUpFacebookButton = findViewById(R.id.signUpFacebookButton);
+        signUpGoogleButton = findViewById(R.id.signUpGoogleButton);
     }
 }
