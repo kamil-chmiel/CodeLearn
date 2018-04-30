413d0bd4f0267056fc9b5026d04d6ae756bb56fe
mdjdrn1 <mdjdrn1@gmail.com>
2018-04-30 22:40:03 +0200

    Updated SignUp and SignIn activities layouts. Added credentials validation on sign up and sign in. Added transitions for those activites.

--- app/src/main/java/com/company/codelearn/SignInActivity.java
+++ app/src/main/java/com/company/codelearn/SignInActivity.java
@@ -1,7 +1,9 @@
 package com.company.codelearn;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -34,6 +36,35 @@ public class SignInActivity extends AppCompatActivity {
         signInGoogleButton = findViewById(R.id.signInGoogleButton);
 
         signInBasicButton.setOnClickListener(view -> {
+            String mail = mailTextField.getText().toString();
+            String password = passwordTextField.getText().toString();
+
+            boolean isFormValid = true;
+
+            if(mail == null || mail.isEmpty()) {
+                mailTextField.setError("E-mail cannot be empty.");
+                isFormValid = false;
+            } else {
+                if(!Patterns.EMAIL_ADDRESS.matcher(mail).matches()) {
+                    mailTextField.setError("E-mail has incorrect format.");
+                    isFormValid = false;
+                }
+            }
+
+            if(password == null || password.isEmpty()) {
+                passwordTextField.setError("Password cannot be empty.");
+                isFormValid = false;
+            } else {
+                if(password.length() < 6) {
+                    passwordTextField.setError("Password is too short.");
+                    isFormValid = false;
+                }
+            }
+
+            if(!isFormValid) {
+                return;
+            }
+
             auth.signInWithEmailAndPassword(mailTextField.getText().toString(), passwordTextField.getText().toString())
                     .addOnCompleteListener(this, authResultTask -> {
                         if (authResultTask.isSuccessful()) {
@@ -41,13 +72,17 @@ public class SignInActivity extends AppCompatActivity {
 //                            Log.d(TAG, "signInWithEmail:success");
                             System.out.println("Success!");
                             FirebaseUser user = auth.getCurrentUser();
-//                            updateUI(user);
+
+                            Intent intent = new Intent(this,
+                                    MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                            startActivity(intent);
                         } else {
                             // If sign in fails, display a message to the user.
 //                            Log.w(TAG, "signInWithEmail:failure", task.getException());
                             Toast.makeText(this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
-//                            updateUI(null);
                         }
                     });
         });
--- app/src/main/java/com/company/codelearn/SignUpActivity.java
+++ app/src/main/java/com/company/codelearn/SignUpActivity.java
@@ -1,7 +1,9 @@
 package com.company.codelearn;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -17,8 +19,7 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText repeatPasswordTextField;
 
     private Button signUpBasicButton;
-    private Button signUpFacebookButton;
-    private Button signUpGoogleButton;
+    private Button signInSocial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,33 +30,90 @@ public class SignUpActivity extends AppCompatActivity {
 
         mailTextField = findViewById(R.id.signUpEmailText);
         passwordTextField = findViewById(R.id.signUpPasswordText);
-        passwordTextField = findViewById(R.id.signUpRepeatPasswordText);
+        repeatPasswordTextField = findViewById(R.id.signUpRepeatPasswordText);
 
         signUpBasicButton = findViewById(R.id.signUpBasicButton);
-        signUpFacebookButton = findViewById(R.id.signUpFacebookButton);
-        signUpGoogleButton = findViewById(R.id.signUpGoogleButton);
+        signInSocial = findViewById(R.id.signInSocial);
 
         signUpBasicButton.setOnClickListener(view -> {
-            auth.createUserWithEmailAndPassword(mailTextField.getText().toString(),
-                                                passwordTextField.getText().toString())
-                .addOnCompleteListener(this, (authResultTask) -> {
-                    if (authResultTask.isSuccessful()) {
-                        // Sign in success, update UI with the signed-in user's information
-//                        Log.d( "createUserWithEmail:success");
-                        System.out.println("Success");
-                        FirebaseUser user = auth.getCurrentUser();
+            String mail = mailTextField.getText().toString();
+            String password = passwordTextField.getText().toString();
+            String repeatPassword = repeatPasswordTextField.getText().toString();
 
-//                        updateUI(user);
-                    } else {
-                        // If sign in fails, display a message to the user.
+            boolean isFormValid = true;
+
+            if(mail == null || mail.isEmpty()) {
+                mailTextField.setError("E-mail cannot be empty.");
+                isFormValid = false;
+            } else {
+                if(!Patterns.EMAIL_ADDRESS.matcher(mail).matches()) {
+                    mailTextField.setError("E-mail has incorrect format.");
+                    isFormValid = false;
+                }
+            }
+
+            if(password == null || password.isEmpty()) {
+                passwordTextField.setError("Password cannot be empty.");
+                isFormValid = false;
+            } else {
+                if(password.length() < 6) {
+                    passwordTextField.setError("Password should have at least 6 characters.");
+                    isFormValid = false;
+                }
+
+                if(repeatPassword == null || repeatPassword.isEmpty()) {
+                    repeatPasswordTextField.setError("Repeat your password.");
+                    isFormValid = false;
+                } else {
+                    if(!password.equals(repeatPassword)) {
+                        repeatPasswordTextField.setError("Passwords are not identical!");
+                        isFormValid = false;
+                    }
+                }
+            }
+
+            if(!isFormValid) {
+                return;
+            }
+
+            // TODO: show loading popup
+            auth.createUserWithEmailAndPassword(mail,
+                    passwordTextField.getText().toString())
+                    .addOnCompleteListener(this, (authResultTask) -> {
+                        if (authResultTask.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+//                        Log.d( "createUserWithEmail:success");
+                            // TODO: store userID in database???
+
+                            Toast.makeText(this, "Account created!",
+                                    Toast.LENGTH_SHORT).show();
+
+                            FirebaseUser user = auth.getCurrentUser();
+
+                            Intent intent = new Intent(this,
+                                    SignInActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                            startActivity(intent);
+
+                            Toast.makeText(this, "Now, you can log in!",
+                                    Toast.LENGTH_LONG).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
 //                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
 //                        System.out.println("Failed!\n\t" + authResultTask.getException());
-                        Toast.makeText(this, "Authentication failed. " + authResultTask.getException(),
-                                Toast.LENGTH_LONG).show();
+                            Toast.makeText(this, "Authentication failed. " + authResultTask.getException(),
+                                    Toast.LENGTH_LONG).show();
 
 //                        updateUI(null);
-                    }
-                });
+                        }
+                    });
+        });
+
+        signInSocial.setOnClickListener(view -> {
+            Intent intent = new Intent(SignUpActivity.this , SignInActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
         });
     }
 
