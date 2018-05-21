bf1a857285a34049983b16ccaa3ea60813aa9d83
mdjdrn1 <mdjdrn1@gmail.com>
2018-05-21 23:14:06 +0200

    Added signing in with Google

--- app/src/main/java/com/company/codelearn/SignInActivity.java
+++ app/src/main/java/com/company/codelearn/SignInActivity.java
@@ -7,6 +7,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 public class SignInActivity extends AppCompatActivity {
     private AccountManager accountManager;
 
@@ -16,6 +27,10 @@ public class SignInActivity extends AppCompatActivity {
     private Button signInBasicButton;
     private Button signInFacebookButton;
     private Button signInGoogleButton;
+
+    // Google sign-in related stuff
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +51,7 @@ public class SignInActivity extends AppCompatActivity {
 
     private void initActions() {
         initSignInBasicButton();
+        iniSignInFacebookButton();
     }
 
     private void initSignInBasicButton() {
@@ -76,5 +92,66 @@ public class SignInActivity extends AppCompatActivity {
                     break;
             }
         });
+    }
+
+    private void iniSignInFacebookButton() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+//                .requestIdToken("667568430002-jrsn43u40nv9544igp1qfvo4ojrtnr8s.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+        signInGoogleButton.setOnClickListener(event -> {
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            System.out.println("SIGN IN!");
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                System.out.println("ApiException " + e.getMessage());
+                // Google Sign In failed, update UI appropriately
+//                Log.w(TAG, "Google sign in failed", e);
+            }
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        System.out.println("\tonStart");
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+                .addOnCompleteListener(this, authResultTask -> {
+                    if (authResultTask.isSuccessful()) {
+                        UserData data = accountManager.getUserData();
+                        Intent intent = new Intent(this, MainActivity.class);
+                        intent.putExtra("UserData", data);
+                        startActivity(intent);
+                    } else {
+                        System.out.println("Failed");
+                    }
+                });
     }
 }
