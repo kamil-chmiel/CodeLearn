a6f1de01744c1a4f93cd194b8bb57eb4d5d9a964
mdjdrn1 <mdjdrn1@gmail.com>
2018-06-14 20:26:15 +0200

    Added ProfileActivity

---
+++ app/src/main/java/com/company/codelearn/ProfileFragment.java
@@ -0,0 +1,68 @@
+package com.company.codelearn;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class ProfileFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public ProfileFragment() {
+    }
+
+    public static ProfileFragment newInstance() {
+        ProfileFragment fragment = new ProfileFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        initEditProfileButton();
+    }
+
+    private void initEditProfileButton() {
+        Button editProfileButton = (Button) getView().findViewById(R.id.editProfileButton);
+
+        editProfileButton.setOnClickListener(view -> {
+            Intent intentEditProfile = new Intent(getActivity(), EditProfile.class);
+            getActivity().startActivity(intentEditProfile);
+        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
